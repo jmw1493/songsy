809d107ca5c44af5b493e95a83ce994c2b1c817e
Jared <weissjmw@gmail.com>
2024-05-26 15:34:44 +0800

    adding function

---
+++ amplify/functions/say-hello/handler.ts
@@ -0,0 +1,8 @@
+import { S3Handler } from "aws-lambda";
+
+export const handler: S3Handler = async (event) => {
+  const objectKeys = event.Records.map((record) => record.s3.object.key);
+  console.log(`Upload handler invoked for objects [${objectKeys.join(", ")}]`);
+};
+
+// songzy-files_BUCKET_NAME
--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,11 +1,16 @@
 import { defineStorage } from "@aws-amplify/backend";
+import { sayHello } from "../functions/say-hello/resource";
 
 export const storage = defineStorage({
   name: "songzy-files",
+  triggers: {
+    onUpload: sayHello,
+  },
   access: (allow) => ({
     "image/*": [
       allow.guest.to(["read"]),
       allow.authenticated.to(["read", "write", "delete"]),
+      allow.resource(sayHello).to(["read", "write", "delete"]),
     ],
     "audio/*": [
       allow.guest.to(["read"]),
