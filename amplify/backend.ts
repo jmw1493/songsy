49ea98325fdd70cd7ca924348dae939d3a115f78
Jared <weissjmw@gmail.com>
2024-05-27 01:20:06 +0800

    gave function proper name

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,11 +2,11 @@ import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource";
 import { data } from "./data/resource";
 import { storage } from "./storage/resource";
-import { sayHello } from "./functions/say-hello/resource";
+import { compressImage } from "./functions/compress-image/resource";
 
 defineBackend({
   auth,
   data,
   storage,
-  sayHello,
+  compressImage,
 });
--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,16 +1,16 @@
 import { defineStorage } from "@aws-amplify/backend";
-import { sayHello } from "../functions/say-hello/resource";
+import { compressImage } from "../functions/compressImage/resource";
 
 export const storage = defineStorage({
   name: "songzy-files",
   triggers: {
-    onUpload: sayHello,
+    onUpload: compressImage,
   },
   access: (allow) => ({
     "image/*": [
       allow.guest.to(["read"]),
       allow.authenticated.to(["read", "write", "delete"]),
-      allow.resource(sayHello).to(["read", "write", "delete"]),
+      allow.resource(compressImage).to(["read", "write", "delete"]),
     ],
     "audio/*": [
       allow.guest.to(["read"]),
