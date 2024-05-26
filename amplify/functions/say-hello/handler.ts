2b80d36ab5f2413d1ee8f80938d6d15aba3a6ded
Jared <weissjmw@gmail.com>
2024-05-26 15:54:36 +0800

    test function filter for image

--- amplify/functions/say-hello/handler.ts
+++ amplify/functions/say-hello/handler.ts
@@ -1,8 +1,22 @@
 import { S3Handler } from "aws-lambda";
 
 export const handler: S3Handler = async (event) => {
+  const imageMimeType = /^image\//;
+
   const objectKeys = event.Records.map((record) => record.s3.object.key);
   console.log(`Upload handler invoked for objects [${objectKeys.join(", ")}]`);
+
+  const uploadedFilesContainImage = event.Records.some((record) => {
+    return imageMimeType.test(record.s3.object.key);
+  });
+
+  if (!uploadedFilesContainImage) {
+    console.log("No images");
+    // If the file is not an image, do nothing
+    return;
+  }
+
+  console.log("Yes images");
 };
 
 // songzy-files_BUCKET_NAME
