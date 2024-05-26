8c9a4415611fbc4018711dfc58ffcb78abe0d795
Jared <weissjmw@gmail.com>
2024-05-26 16:55:51 +0800

    baby stepping image compression - half way there

--- amplify/functions/say-hello/handler.ts
+++ amplify/functions/say-hello/handler.ts
@@ -1,4 +1,8 @@
 import { S3Handler } from "aws-lambda";
+import AWS from "aws-sdk";
+
+const s3 = new AWS.S3();
+const bucketName = process.env.SONGZY_FILES_BUCKET_NAME ?? "";
 
 export const handler: S3Handler = async (event) => {
   const imageMimeType = /^image\//;
@@ -17,6 +21,23 @@ export const handler: S3Handler = async (event) => {
   }
 
   console.log("Yes images");
+  console.log("bucket name", bucketName);
+
+  for (const record of event.Records) {
+    const key = record.s3.object.key;
+    if (!imageMimeType.test(key)) {
+      console.log(`File ${key} is not an image, skipping...`);
+      continue;
+    }
+    try {
+      const s3Object = await s3
+        .getObject({ Bucket: bucketName, Key: key })
+        .promise();
+      console.log("s3Object", s3Object);
+    } catch (e) {
+      console.error(e);
+    }
+  }
 };
 
 // songzy-files_BUCKET_NAME
